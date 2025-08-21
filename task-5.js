e0a3c98cc8f0171eb7d10b0c34fc6a6d3bc8b916
bdahsan1999wd <bdahsan1999wd@gmail.com>
2025-08-21 12:49:57 +0600

    task-5 added

---
+++ task-5.js
@@ -0,0 +1,11 @@
+function odd_even(num) {
+    if (num % 2 === 0) {
+        return "Even";
+    }
+    else {
+        return "Odd";
+    }
+}
+
+console.log(odd_even(9));
+console.log(odd_even(10));
